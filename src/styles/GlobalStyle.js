0e21ddd38846746e12260d840f7ce9df879f9edf
이름 <ktg6361@gmail.com>
2021-10-25 18:09:42 +0900

    modify: 초기세팅 수정

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -2,7 +2,11 @@ import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
 
 const GlobalStyle = createGlobalStyle`
-  ${reset}	
+  ${reset}
+  
+  * {
+    box-sizing: border-box;
+  }
 `;
 
 export default GlobalStyle;
