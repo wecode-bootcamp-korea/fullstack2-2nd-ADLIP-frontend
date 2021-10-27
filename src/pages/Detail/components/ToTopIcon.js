5432b70c188bd2494494d2d95dacbeadb38fff6a
jin0oon <86861297+jin0oon@users.noreply.github.com>
2021-10-27 15:24:51 +0900

    Modify: floating bar 추가, ToTopButton 추가, styled components 리팩토링, mockdata json수정 후 rebase 완료

---
+++ src/pages/Detail/components/ToTopIcon.js
@@ -0,0 +1,20 @@
+export const ToTopIcon = () => {
+  return (
+    <svg
+      xmlns='http://www.w3.org/2000/svg'
+      width='24'
+      height='24'
+      fill='none'
+      viewBox='0 0 24 24'
+    >
+      <path
+        stroke='rgb(0, 60, 204)'
+        strokeOpacity='20%'
+        strokeLinecap='round'
+        strokeLinejoin='round'
+        strokeWidth='2'
+        d='M18.75 14.25L12 7.5l-6.75 6.75'
+      />
+    </svg>
+  );
+};
