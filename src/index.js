beeae12900a6de6eb51e4385a6c80dd03cdb34ef
jin0oon <86861297+jin0oon@users.noreply.github.com>
2021-11-04 15:47:51 +0900

    Modify: 로고 수정, 파비콘 수정, 라우터 예전 상태로 되돌림, 카카오 키 수정 (#33)

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Router from './Routes';
-import GlobalStyle from './styles/GlobalStyle';
-import { ThemeProvider } from 'styled-components';
-import theme from './styles/theme';
-import { ModalProvider } from 'styled-react-modal';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <GlobalStyle>
-        <ThemeProvider theme={theme}>
-          <ModalProvider></ModalProvider>
-        </ThemeProvider>
-      </GlobalStyle>
-    </Router>
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
